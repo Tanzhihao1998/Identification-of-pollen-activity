dd532ec7c4559e0d60bb3a5d2d7ad2a580aa4d1a
7777777 <840703721@qq.com>
2021-12-29 16:32:15 +0800

    -

---
+++ 1.py
@@ -0,0 +1,16 @@
+from playsound import playsound
+
+playsound(r"F:/人脸识别/yolov5-master2/1.mp3")
+# import os
+# file=r"F:\人脸识别\yolov5-master2\1.mp3"
+# os.system(file)
+# import pygame
+# pygame.mixer.init()
+#
+# track = pygame.mixer.music.load(r"F:\人脸识别\yolov5-master2\1.mp3")
+# # pygame.mixer.music.play()
+# #
+# # pygame.mixer.music.pause() #暂停
+# # pygame.mixer.music.unpause()#取消暂停
+# import winsound
+# winsound.PlaySound(r"F:\人脸识别\yolov5-master2\1.mp3",winsound.SND_FILENAME)
